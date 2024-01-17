93d7f4a3e96edd9143d05f5707d9c7a76b9f919a
Blake Hill <blake.h.hill09@gmail.com>
2024-01-16 19:24:27 -0700

    testoftwo

---
+++ maxOfTwo.js
@@ -0,0 +1,17 @@
+let x = 3
+let y = 4
+
+// Write your code below this line.
+/* if (x > y) {
+    console.log(x)
+} else {
+    console.log(y)
+}
+ */
+// bonus quesetion
+if (x > y) 
+    console.log(x);
+else if (x < y)
+    console.log(y)
+else 
+    console.log("the values are identical")
